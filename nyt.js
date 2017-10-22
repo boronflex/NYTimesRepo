019cc29c4a1338f54ad0bb72932331c2b12e7903
boronflex <kyle_palmer@icloud.com>
2017-10-21 19:59:15 -0500

    javascript started

--- nyt.js
+++ nyt.js
@@ -1,3 +1,14 @@
 var key = "24ad0c2914ee4ca7983d996ef1128800"
 
-stuff
+
+var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+
+$.ajax({
+	url: queryURL,
+	method: "GET"
+}).done(function(response){
+
+	console.log(response);
+
+
+})
